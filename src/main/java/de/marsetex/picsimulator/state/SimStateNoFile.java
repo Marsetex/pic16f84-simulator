6c9c1c6c6fa37eedfcfe03fc899681f648b2cb60
Marcel Gruessinger <marcel.gruessinger@web.de>
2019-04-08 22:49:52 +0200

    Completed TODOs

--- src/main/java/de/marsetex/picsimulator/state/SimStateNoFile.java
+++ src/main/java/de/marsetex/picsimulator/state/SimStateNoFile.java
@@ -1,8 +1,12 @@
 package de.marsetex.picsimulator.state;
 
 import de.marsetex.picsimulator.Simulator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SimStateNoFile implements ISimState {
+
+	private static final Logger LOGGER = LogManager.getLogger(SimStateNoFile.class);
 
 	@Override
 	public boolean isTransitionAllowed(ISimState state) {
@@ -14,11 +18,11 @@ public class SimStateNoFile implements ISimState {
 
 	@Override
 	public void onEnteringState(Simulator simulator) {
-		// TODO: Logger
+		LOGGER.info("Entering state 'SimStateNoFile'");
 	}
 
 	@Override
 	public void onLeavingState(Simulator simulator) {
-		// TODO: Logger
+		LOGGER.info("Leaving state 'SimStateNoFile'");
 	}
 }
