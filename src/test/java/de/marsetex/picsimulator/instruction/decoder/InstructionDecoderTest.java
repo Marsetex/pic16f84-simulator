206a92063bd1dc473e2bb091edd697223998916e
Marcel Gruessinger <marcel.gruessinger@web.de>
2019-04-09 00:04:46 +0200

    Implemented tests for decoding instructions with prefix 0b011

---
+++ src/test/java/de/marsetex/picsimulator/instruction/decoder/InstructionDecoderTest.java
@@ -0,0 +1,61 @@
+package de.marsetex.picsimulator.instruction.decoder;
+
+import de.marsetex.picsimulator.instruction.*;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InstructionDecoderTest {
+
+    private final InstructionDecoder decoder = new InstructionDecoder();
+
+    @Test
+    public void testDecodeMovlw() {
+        String movlwOpcode = "3011";
+        IInstruction instruction = decoder.decode(Short.parseShort(movlwOpcode, 16));
+        assertThat(instruction, IsInstanceOf.instanceOf(Movlw.class));
+    }
+
+    @Test
+    public void testDecodeRetlw() {
+        // String movlwOpcode = "3011";
+        // IInstruction instruction = decoder.decode(Short.parseShort(opCodeAsString, 16));
+        // assertThat(instruction, IsInstanceOf.instanceOf(Movlw.class));
+    }
+
+    @Test
+    public void testDecodeIorlw() {
+        String iorlwOpcode = "380D";
+        IInstruction instruction = decoder.decode(Short.parseShort(iorlwOpcode, 16));
+        assertThat(instruction, IsInstanceOf.instanceOf(Iorlw.class));
+    }
+
+    @Test
+    public void testDecodeAndlw() {
+        String andlwOpcode = "3930";
+        IInstruction instruction = decoder.decode(Short.parseShort(andlwOpcode, 16));
+        assertThat(instruction, IsInstanceOf.instanceOf(Andlw.class));
+    }
+
+    @Test
+    public void testDecodeXorlw() {
+        String xorlwOpcode = "3A20";
+        IInstruction instruction = decoder.decode(Short.parseShort(xorlwOpcode, 16));
+        assertThat(instruction, IsInstanceOf.instanceOf(Xorlw.class));
+    }
+
+    @Test
+    public void testDecodeSublw() {
+        String sublwOpcode = "3C3D";
+        IInstruction instruction = decoder.decode(Short.parseShort(sublwOpcode, 16));
+        assertThat(instruction, IsInstanceOf.instanceOf(Sublw.class));
+    }
+
+    @Test
+    public void testDecodeAddlw() {
+        String addlwOpcode = "3E25";
+        IInstruction instruction = decoder.decode(Short.parseShort(addlwOpcode, 16));
+        assertThat(instruction, IsInstanceOf.instanceOf(Addlw.class));
+    }
+}
