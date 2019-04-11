f52ab57a578392e79428453979bb5c145183d76d
Marcel Grüßinger <marcel.gruessinger@web.de>
2019-04-11 12:01:49 +0200

    Implemented all special registers

--- src/main/java/de/marsetex/picsimulator/microcontroller/PIC16F84.java
+++ src/main/java/de/marsetex/picsimulator/microcontroller/PIC16F84.java
@@ -3,15 +3,34 @@ package de.marsetex.picsimulator.microcontroller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.List;
-
 public class PIC16F84 {
 
     private static final Logger LOGGER = LogManager.getLogger(PIC16F84.class);
 
     private short[] programMemory = new short[8192];
 
+    private byte[] ramBank0 = new byte[128];
+    private byte[] ramBank1 = new byte[128];
+
     private byte wRegister;
+    private int programCounter = 0;
+
+    private byte portA;
+    private byte portB;
+    private byte trisA;
+    private byte trisB;
+    private byte option;
+    private byte pclath;
+    private byte intcon;
+    private byte fsr;
+    private byte status;
+    private byte pcl;
+    private byte tmr0;
+    private byte ind;
+
+    public short getNextInstruction() {
+        return programMemory[programCounter];
+    }
 
     public void loadOpcodeIntoProgramMemory(String addressOfOpcode, String opcode) {
         programMemory[Short.parseShort(addressOfOpcode, 16)] = Short.parseShort(opcode, 16);
@@ -24,5 +43,13 @@ public class PIC16F84 {
 
     public void setWRegister(byte wRegister) {
         this.wRegister = wRegister;
+    }
+
+    public void resetProgramCounter() {
+        programCounter = 0;
+    }
+
+    public void incrementProgramCounter() {
+        programCounter++;
     }
 }
