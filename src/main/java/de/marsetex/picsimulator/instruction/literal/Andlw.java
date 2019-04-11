3de87187596bd65738c53812b057898ade7545f5
Marcel Grüßinger <marcel.gruessinger@web.de>
2019-04-11 12:25:53 +0200

    Implemented Andlw

--- src/main/java/de/marsetex/picsimulator/instruction/literal/Andlw.java
+++ src/main/java/de/marsetex/picsimulator/instruction/literal/Andlw.java
@@ -5,8 +5,14 @@ import de.marsetex.picsimulator.microcontroller.PIC16F84;
 
 public class Andlw implements IPicInstruction {
 
+    private int k;
+
+    public Andlw(short opcode) {
+        k = opcode & 0x00FF;
+    }
+
     @Override
     public void execute(PIC16F84 pic) {
-
+        pic.setWRegister((byte) ((byte) k & pic.getWRegister()));
     }
 }
