f67618702fcf720a570244f348133cab14e95c35
Marcel Gruessinger <marcel.gruessinger@web.de>
2019-04-12 17:47:58 +0200

    Implemented GOTO

--- src/main/java/de/marsetex/picsimulator/instruction/control/Goto.java
+++ src/main/java/de/marsetex/picsimulator/instruction/control/Goto.java
@@ -3,10 +3,20 @@ package de.marsetex.picsimulator.instruction.control;
 import de.marsetex.picsimulator.instruction.IPicInstruction;
 import de.marsetex.picsimulator.microcontroller.PIC16F84;
 
+/**
+ * Go to address
+ * Datasheet: Page 62
+ */
 public class Goto implements IPicInstruction {
+
+    private short address;
+
+    public Goto(short opcode) {
+        address = (short) (opcode & 0x07FF);
+    }
 
     @Override
     public void execute(PIC16F84 pic) {
-
+        pic.setProgramCounterValue(address);
     }
 }
