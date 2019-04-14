4e3cb22c7abc0fe9272b63e8e2b6bc06b177f781
Marcel Gruessinger <marcel.gruessinger@web.de>
2019-04-14 13:04:13 +0200

    Implemented RETURN

--- src/main/java/de/marsetex/pic16f84sim/instruction/control/Return.java
+++ src/main/java/de/marsetex/pic16f84sim/instruction/control/Return.java
@@ -2,11 +2,20 @@ package de.marsetex.pic16f84sim.instruction.control;
 
 import de.marsetex.pic16f84sim.instruction.IPicInstruction;
 import de.marsetex.pic16f84sim.microcontroller.PIC16F84;
+import de.marsetex.pic16f84sim.microcontroller.memory.Stack;
+import de.marsetex.pic16f84sim.microcontroller.register.ProgramCounter;
 
+/**
+ * Return from Subroutine
+ * Datasheet: Page 66
+ */
 public class Return implements IPicInstruction {
 
     @Override
     public void execute(PIC16F84 pic) {
+        ProgramCounter pc = pic.getProgramCounter();
+        Stack stack = pic.getStack();
 
+        pc.setProgramCounter(stack.pop());
     }
 }
