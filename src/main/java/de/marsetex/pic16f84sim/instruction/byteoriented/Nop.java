ec366faa1775eebbd9aeed1e33e1253cfa1661af
Marcel Gruessinger <marcel.gruessinger@web.de>
2019-04-14 13:22:45 +0200

    Implemented NOP

--- src/main/java/de/marsetex/pic16f84sim/instruction/byteoriented/Nop.java
+++ src/main/java/de/marsetex/pic16f84sim/instruction/byteoriented/Nop.java
@@ -3,10 +3,14 @@ package de.marsetex.pic16f84sim.instruction.byteoriented;
 import de.marsetex.pic16f84sim.instruction.IPicInstruction;
 import de.marsetex.pic16f84sim.microcontroller.PIC16F84;
 
+/**
+ * No Operation
+ * Datasheet: Page 65
+ */
 public class Nop implements IPicInstruction {
 
     @Override
     public void execute(PIC16F84 pic) {
-
+        // Do noting
     }
 }
