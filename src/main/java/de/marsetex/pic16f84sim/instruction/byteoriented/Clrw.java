46f31179a3f0c23be4f0ea803268df6c80cb1af3
Marcel Gruessinger <marcel.gruessinger@web.de>
2019-04-16 00:16:29 +0200

    Implemented CLRW

--- src/main/java/de/marsetex/pic16f84sim/instruction/byteoriented/Clrw.java
+++ src/main/java/de/marsetex/pic16f84sim/instruction/byteoriented/Clrw.java
@@ -1,12 +1,19 @@
 package de.marsetex.pic16f84sim.instruction.byteoriented;
 
-import de.marsetex.pic16f84sim.instruction.IPicInstruction;
+import de.marsetex.pic16f84sim.instruction.StatusFlagChangerInstruction;
 import de.marsetex.pic16f84sim.microcontroller.PIC16F84;
+import de.marsetex.pic16f84sim.microcontroller.register.WRegister;
 
-public class Clrw implements IPicInstruction {
+/**
+ * Clear W
+ * Datasheet: Page 60
+ */
+public class Clrw extends StatusFlagChangerInstruction {
 
     @Override
     public void execute(PIC16F84 pic) {
+        isValueEqualsZero((byte) 0);
 
+        pic.getWRegister().setWRegisterValue((byte) 0);
     }
 }
