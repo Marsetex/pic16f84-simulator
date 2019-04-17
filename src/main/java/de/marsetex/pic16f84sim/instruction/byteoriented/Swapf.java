d7999c5293092f25e82e685450c1b97cf63021c4
Marcel Gruessinger <marcel.gruessinger@web.de>
2019-04-17 17:49:56 +0200

    Implemented SWAPF

--- src/main/java/de/marsetex/pic16f84sim/instruction/byteoriented/Swapf.java
+++ src/main/java/de/marsetex/pic16f84sim/instruction/byteoriented/Swapf.java
@@ -2,6 +2,8 @@ package de.marsetex.pic16f84sim.instruction.byteoriented;
 
 import de.marsetex.pic16f84sim.instruction.IPicInstruction;
 import de.marsetex.pic16f84sim.microcontroller.PIC16F84;
+import de.marsetex.pic16f84sim.microcontroller.memory.DataMemory;
+import de.marsetex.pic16f84sim.microcontroller.register.WRegister;
 
 /**
  * Swap nibbles in f
@@ -19,6 +21,19 @@ public class Swapf implements IPicInstruction {
 
     @Override
     public void execute(PIC16F84 pic) {
+        DataMemory dataMemory = pic.getDataMemory();
 
+        byte fValue = dataMemory.load(fileRegister);
+
+        byte upperBits = (byte) (fValue << 4);
+        byte lowerBits = (byte) (fValue >> 4);
+
+        fValue = (byte) (lowerBits | upperBits);
+
+        if(destination == 0) {
+            pic.getWRegister().setWRegisterValue(fValue);
+        } else {
+            dataMemory.store(fileRegister, fValue);
+        }
     }
 }
