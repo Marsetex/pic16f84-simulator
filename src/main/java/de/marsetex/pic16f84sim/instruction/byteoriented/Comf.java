7ac9e086b387c949d3e02ff83877ca32eb46503e
Marcel Gruessinger <marcel.gruessinger@web.de>
2019-04-17 17:58:12 +0200

    Implemented COMF

--- src/main/java/de/marsetex/pic16f84sim/instruction/byteoriented/Comf.java
+++ src/main/java/de/marsetex/pic16f84sim/instruction/byteoriented/Comf.java
@@ -2,6 +2,8 @@ package de.marsetex.pic16f84sim.instruction.byteoriented;
 
 import de.marsetex.pic16f84sim.instruction.StatusFlagChangerInstruction;
 import de.marsetex.pic16f84sim.microcontroller.PIC16F84;
+import de.marsetex.pic16f84sim.microcontroller.memory.DataMemory;
+import de.marsetex.pic16f84sim.microcontroller.register.WRegister;
 
 /**
  * Complement f. Sets flag: Z
@@ -19,6 +21,17 @@ public class Comf extends StatusFlagChangerInstruction {
 
     @Override
     public void execute(PIC16F84 pic) {
+        DataMemory dataMemory = pic.getDataMemory();
 
+        byte fValue = dataMemory.load(fileRegister);
+        fValue = (byte) ~fValue;
+
+        isValueEqualsZero(fValue);
+
+        if(destination == 0) {
+            pic.getWRegister().setWRegisterValue(fValue);
+        } else {
+            dataMemory.store(fileRegister, fValue);
+        }
     }
 }
