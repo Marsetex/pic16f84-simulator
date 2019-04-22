55ba6dbb1a8afd4b0f7c3e93f8f53f3b7d868f1a
Marcel Gruessinger <marcel.gruessinger@web.de>
2019-04-22 12:59:53 +0200

    Split up content of SimulatorUIComponents class

--- src/main/java/de/marsetex/pic16f84sim/ui/components/AboutDialog.java
+++ src/main/java/de/marsetex/pic16f84sim/ui/components/AboutDialog.java
@@ -1,4 +1,18 @@
 package de.marsetex.pic16f84sim.ui.components;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 public class AboutDialog {
+
+    public static Alert getAboutDialog() {
+        Alert alert = new Alert(AlertType.INFORMATION);
+
+        alert.setTitle("PIC16F84 Simulator - About");
+        alert.setHeaderText("PIC16F84 Simulator");
+        alert.setContentText("Developed by: Marcel Gruessinger \n" +
+                "Version: 1.0.0");
+
+        return alert;
+    }
 }
