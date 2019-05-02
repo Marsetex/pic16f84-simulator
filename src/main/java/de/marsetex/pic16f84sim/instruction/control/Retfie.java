52940bdc752505d45e2452bb8ffad5f7947ec9a6
Marcel Gruessinger <marcel.gruessinger@web.de>
2019-05-02 23:36:38 +0200

    Implemented RETIFIE

--- src/main/java/de/marsetex/pic16f84sim/instruction/control/Retfie.java
+++ src/main/java/de/marsetex/pic16f84sim/instruction/control/Retfie.java
@@ -2,11 +2,28 @@ package de.marsetex.pic16f84sim.instruction.control;
 
 import de.marsetex.pic16f84sim.instruction.IPicInstruction;
 import de.marsetex.pic16f84sim.microcontroller.PIC16F84;
+import de.marsetex.pic16f84sim.microcontroller.memory.DataMemory;
+import de.marsetex.pic16f84sim.microcontroller.memory.Stack;
+import de.marsetex.pic16f84sim.microcontroller.register.ProgramCounter;
 
+/**
+ * Return from interrupt
+ * Datasheet: Page 65
+ */
 public class Retfie implements IPicInstruction {
 
     @Override
     public int execute(PIC16F84 pic) {
+        DataMemory dataMemory = pic.getDataMemory();
+        ProgramCounter pc = pic.getProgramCounter();
+        Stack stack = pic.getStack();
+
+        byte intconValue = dataMemory.load((byte) 0x0B);
+        intconValue = (byte) (intconValue | 0b10000000);
+        dataMemory.store((byte) 0x0B, intconValue);
+
+        pc.setProgramCounterValue(stack.pop());
+
         return 2;
     }
 }
